b27afc0c79c860aee9aba56c1a1f01d81881bacc
XiVan <gomkai@hotmail.fr>
2024-11-19 17:39:37 +0100

    adress

---
+++ src/Address.jsx
@@ -0,0 +1,18 @@
+
+
+const address = {
+    lastName : "Doe",
+    firstName : "John",
+    age : "25",
+    phone : "01 01 01 01 01", 
+    mail : "john@doe.com" ,
+    addr : {
+        city : "Paris",
+        street : "3 Rue du paradis",
+        code : "75000"
+    }
+}
+
+export default address;
+
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+import Home from './pages/Home';
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import Contact from './pages/Contact';
+import Mentions from "./pages/Mentions";
+import Footer from "./Footer";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import address from './Address';
 
-function App() {
-  return (
+
+export default function App(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav class="navbar navbar-expand-lg bg-dark text-light text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand text-light fw-bolder" href="#page-top">{address.firstName} {address.lastName}</a>
+                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive"
+                aria-expanded="false"
+                aria-label="Toggle navigation"><i class="fas fa-bars"></i>
+                </button>
+
+                <div class="collapse navbar-collapse"id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <Link to="/"className="nav-item mx-0 mx-lg-1 text-decoration-none text-light">Accueil</Link>
+                        <Link to="/services"className="nav-item mx-0 mx-lg-1 text-decoration-none text-light">Services</Link>
+                        <Link to="/portfolio"className="nav-item mx-0 mx-lg-1 text-decoration-none text-light">Portfolio</Link>
+                        <Link to="/contact" className="nav-item mx-0 mx-lg-1 text-decoration-none text-light">Contact</Link>
+                        <Link to="/Mentions"className="nav-item mx-0 mx-lg-1 text-decoration-none text-light">Mentions légales</Link>
+                    </ul>
+                </div>
+            </div>
+        </nav>
       </header>
+
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/services" element={<Services/>}></Route>
+        <Route path="/portfolio" element={<Portfolio/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/mentions" element={<Mentions/>}></Route>
+      </Routes>
+
+      <Footer />
+      
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
     </div>
+    
   );
 }
-
-export default App;
